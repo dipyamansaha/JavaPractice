00cf61da9675357057be252e7f7f6a81fd4d708c
Dipyaman Saha <dipyaman.work@gmail.com>
2020-05-01 18:53:34 +0530

    iterators and basics of maze

--- src/pack2/MyCuteProgram.java
+++ src/pack2/MyCuteProgram.java
@@ -61,6 +61,59 @@ public class MyCuteProgram
         System.out.println(names.removeFirst());
     }
 
+    void iteratorOperations()
+    {
+        LinkedList<String> names = new LinkedList<String>();
+        names.push("Me2");
+        names.push("You2");
+        names.push("Somebody else2");
+
+        Iterator<String> it = names.iterator();
+
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+
+        for (String s: names)
+        {
+            System.out.println(s);
+        }
+
+        names.add(2, "Susan");
+        for(String s: names)
+        {
+            System.out.println(s);
+        }
+
+        ListIterator<String> it2 = names.listIterator();
+        it2.next();
+        it2.next();
+        it2.add("Sally");
+        for(String s: names)
+        {
+            System.out.println(s);
+        }
+    }
+
+    void array2DThings()
+    {
+        int[][] data = {
+                {4, 6, 3, 10},
+                {4, 2, 40, 1, 5, 3, 2, 30},
+                {5, 34, 1, 43}
+        };
+
+        for(int i = 0; i < data.length; i++)
+        {
+            for(int j = 0; j < data[i].length; j++)
+            {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException
     {
         MyCuteProgram myCuteProgram = new MyCuteProgram();
@@ -70,8 +123,8 @@ public class MyCuteProgram
         System.out.println("Enter 2: inputFromFile2()");
         System.out.println("Enter 3: llAsQ()");
         System.out.println("Enter 4: llAsStack()");
-        System.out.println("Enter 5: ");
-        System.out.println("Enter 6: ");
+        System.out.println("Enter 5: iteratorOperations()");
+        System.out.println("Enter 6: array2DThings()");
         System.out.println("Enter 7: ");
         System.out.println("Enter 8: ");
         System.out.println("Enter 9: ");
@@ -81,7 +134,7 @@ public class MyCuteProgram
 
         int num = sc.nextInt();
 
-        switch (num)
+        switch(num)
         {
             case 1 -> {
                 System.out.println("Calling Method-1... \n");
@@ -100,7 +153,12 @@ public class MyCuteProgram
                 myCuteProgram.llAsStack();
             }
             case 5 -> {
-                
+                System.out.println("Calling Method-5... \n");
+                myCuteProgram.iteratorOperations();
+            }
+            case 6 -> {
+                System.out.println("Calling Method-6... \n");
+                myCuteProgram.array2DThings();
             }
             default -> System.out.println("\nTry again later!\n");
         }
---
+++ src/pack3/MazeSolver.java
@@ -0,0 +1,25 @@
+package pack3;
+
+import java.util.LinkedList;
+
+public class MazeSolver
+{
+    static int[][] maze = {
+            {2, 0, 1, 1},
+            {1, 1, 0, 1},
+            {0, 1, 1, 1}
+    };
+    //0 = Wall
+    //1 = Path
+    //2 = Destination
+
+    static LinkedList<Position> path = new LinkedList<Position>();
+
+    public static void main(String[] args)
+    {
+        Position p = new Position(3, 0);
+        path.push(p);
+
+        maze[path.peek().y][path.peek().x] = 0;
+    }
+}
